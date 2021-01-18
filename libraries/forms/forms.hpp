a32be0f3389f04c4eb64486ab6816a42143abebb
bvi <coffe.bezugly@gmail.com>
2021-01-18 11:30:48 +0300

    implemented class Plot

--- libraries/forms/forms.hpp
+++ libraries/forms/forms.hpp
@@ -35,6 +35,7 @@ public:
     }
     Form& show(cv::Mat m)
     {
+        m_ = m;
         cv::imshow(title_, m);
 
         return *this;
@@ -69,10 +70,10 @@ protected:
     {
         switch(event)
         {
-            case cv::EVENT_LBUTTONDOWN: on_lbutton_down(x, y, flags);
-            case cv::EVENT_RBUTTONDOWN: on_rbutton_down(x, y, flags);
-            case cv::EVENT_MBUTTONDOWN: on_mbutton_down(x, y, flags);
-            case cv::EVENT_MOUSEMOVE:   on_mouse_move(x, y, flags);
+            case cv::EVENT_LBUTTONDOWN: on_lbutton_down(x, y, flags); break;
+            case cv::EVENT_RBUTTONDOWN: on_rbutton_down(x, y, flags); break;
+            case cv::EVENT_MBUTTONDOWN: on_mbutton_down(x, y, flags); break;
+            case cv::EVENT_MOUSEMOVE:   on_mouse_move(x, y, flags);   break;
         };
     }
 protected:
@@ -80,4 +81,57 @@ protected:
     cv::Mat m_;
 };
 
-}//namespace nv:;ui
+template<typename T>
+class Plot
+{
+public:
+    Plot(std::string const& title, cv::Size const& size, cv::Scalar const& background = cv::Scalar().all(0))
+        : size_(size), m_(size, CV_8UC3, background), title_(title) 
+    {
+    }
+    void draw_caption(std::string const& caption)
+    {
+        int fontFace = cv::FONT_HERSHEY_SCRIPT_SIMPLEX;
+        double fontScale = 1;
+        int thickness = 1;
+        int baseline=0;
+        cv::Size textSize = cv::getTextSize(caption, fontFace,
+                                    fontScale, thickness, &baseline);
+        baseline += thickness;        
+
+        cv::Point textOrg((m_.cols - textSize.width)/2, textSize.height + 2);// (m_.rows + textSize.height)/2);
+        cv::putText(m_, caption, textOrg, fontFace, fontScale, cv::Scalar::all(128), thickness, 8);        
+
+    }
+    void draw_grid(int grid_size, cv::Scalar const& grid_color = cv::Scalar().all(24))
+    {
+        for(int y = m_.rows; y > 0; y -= grid_size)
+        {
+            cv::line(m_, {0, y}, {m_.cols, y}, grid_color, 1, cv::LINE_AA);
+        }
+        for(int x = 0; x < m_.cols; x += grid_size)
+        {
+            cv::line(m_, {x, 0}, {x, m_.rows}, grid_color, 1, cv::LINE_AA);
+        }        
+    }
+    void draw(std::vector<T> const& values, cv::Scalar const& color, int thickness = 1, float x_scale = 1., float y_scale = 1.)
+    {
+        cv::Point prev_point(0, m_.rows - values[0]*y_scale);
+        for(size_t i = 1; i < values.size(); ++i)
+        {
+            cv::Point curr_point(x_scale*i, m_.rows - values[i]*y_scale);
+            cv::line(m_, prev_point, curr_point, color, thickness);
+            prev_point = curr_point;
+        }
+    }
+    cv::Mat const& image() const
+    {
+        return m_;
+    }
+protected:
+    cv::Size size_;
+    cv::Mat m_;
+    std::string title_;
+};
+
+} //namespace nv::ui
--- samples/matching_tool/main.cpp
+++ samples/matching_tool/main.cpp
@@ -10,15 +10,19 @@ using namespace std;
 class ToolWidow: public nv::ui::Form
 {
 public:
-    ToolWidow(std::string const& title, cv::Mat const &rm, cv::Mat &dm, cv::Mat &sm): nv::ui::Form(title), lm_(m_), rm_(rm), dm_(dm), sm_(sm){};
-    void on_rbutton_down(int x, int y, int)
+    ToolWidow(std::string const& title, cv::Mat const &rm, cv::Mat &dm, cv::Mat &sm): nv::ui::Form(title), rm_(rm), dm_(dm), sm_(sm){};
+    void on_rbutton_down(int x, int y, int) override
     {
-        nv::find_matching_point(lm_, rm_, cv::Point(x, y));
+        auto right_point = nv::find_matching_point(m_, rm_, cv::Point(x, y));
 
-      
+        auto view = rm_.clone();
+        cv::circle(view, right_point, 10, 0, 2);
+        cv::namedWindow("Right Point", cv::WINDOW_NORMAL);
+        cv::imshow("Right Point", view);
+        cv::waitKey(0);
+        cv::destroyWindow("Right Point");
     }
 protected:
-    cv::Mat &lm_;
     cv::Mat const &rm_;
     cv::Mat dm_;
     cv::Mat sm_;
@@ -31,29 +35,32 @@ int main(void)
 {
     try
     {
-        auto lm = cv::imread(resource_path + "moto/im0.png", 0);
-        if(lm.empty()) throw std::system_error(errno, std::system_category(), "moto/im0.png");
-        auto rm = cv::imread(resource_path + "moto/im1.png", 0);
-        if(rm.empty()) throw std::system_error(errno, std::system_category(), "moto/im1.png");
-
-        imshow("LEFT: ", lm);
-        imshow("RIGHT: ", rm);
-
+        std::vector<int> arr{1,3,2,4,6,5,8,7,10,9,13,11,12,17,14,15,16};
+        nv::ui::Plot<int> plot("PLOT", cv::Size(640/2, 480/2));
+        plot.draw_grid(5);
+        plot.draw(arr, cv::Scalar(255, 0, 0), 1, 5, 5);
+        plot.draw_caption("some graph.");
+        cv::namedWindow("PLOT_WINDOW", 0);// cv::WINDOW_NORMAL);
+        cv::imshow("PLOT_WINDOW", plot.image());
         cv::waitKey(0);
+        cv::destroyWindow("PLOT_WINDOW");
 
-        cv::destroyAllWindows();
+        // auto lm = cv::imread(resource_path + "moto/im0.png", 0);
+        // if(lm.empty()) throw std::system_error(errno, std::system_category(), "moto/im0.png");
+        // auto rm = cv::imread(resource_path + "moto/im1.png", 0);
+        // if(rm.empty()) throw std::system_error(errno, std::system_category(), "moto/im1.png");
+        // auto dm = rm.clone();
+        // auto sm = lm.clone();
+        // ToolWidow tool("TOOL", rm, dm, sm);
+        // tool.show(lm);
 
-        auto depth_map = nv::build_depth_map_multy_line(lm, rm);
-
-        cv::namedWindow("DEPTH", cv::WINDOW_NORMAL);
-        cv::imshow("DEPTH", depth_map);
-
-        cv::waitKey(0);        
+        // cv::waitKey(0);
     }
     catch(exception const& ex){
         cout << "fail: " << ex.what() << endl;
     }
 
+    std::cout << "press enter for exit ...\n";
     getchar();
 
     return 0;
